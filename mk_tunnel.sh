f78edb62130dd98b2bd9e0b25cadbc268a8da747
ales stibal <astib@mag0.net>
2026-09-04 18:15:39 +0200

    remove redundant '-6' from ping command

--- mk_tunnel.sh
+++ mk_tunnel.sh
@@ -628,7 +628,7 @@ run_hook_local "$tuntom_post_hook" post up local "$client_if" "$client_ip" "$ser
 
 echo "[11] Test"
 if "${root_cmd[@]}" ping -c 3 -W 2 "$server_ip"; then
-    if "${root_cmd[@]}" ping -6 -c 3 -W 2 "$server_ipv6"; then
+    if "${root_cmd[@]}" ping -c 3 -W 2 "$server_ipv6"; then
         echo "Tunnel is UP (IPv4 + IPv6)"
     else
         echo "IPv6 ping failed"
